b502908af3a0aaa556f623fe871c8e007c0d0fea
Trịnh Văn Đường <152875418+duong204736@users.noreply.github.com>
2024-10-22 21:35:11 +0700

    Revert "Update to #main"

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,7 @@
-import { Routes, Route } from 'react-router-dom'
-import { Header,Home,Login } from './containers/Public';
-import { path } from './untils/constant';
-
-
-
-
 function App() {
   return (
-    <div className="h-screen w-screen bg-primary">
-      <Routes>
-        <Route path={path.HOME} element={< Home/>}>
-          <Route path={path.LOGIN} element={<Login />}/>
-        </Route>
-      </Routes>
+    <div className="bg-gray-600">
+      app
     </div>
   );
 }
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,18 +4,7 @@ module.exports = {
             './public/index.html'
   ],
   theme: {
-    extend: {
-      width : {
-        '1100' : '1100px' // chieu rong cua login 
-      },
-      backgroundColor: {
-        primary: '#C0C0C0',
-        secondary1: '#1266dd',
-        secondary2: '#f73859',
-        'overlay-30': 'rgba(0,0,0,0.3)',
-        'overlay-70': 'rgba(0,0,0,0.7)',
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
