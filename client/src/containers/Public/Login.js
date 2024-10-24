97a1ad5a255c2458c89d9601911da5eb3e54719a
Duongneee <duongquach2k3@gmail.com>
2024-10-24 23:22:27 +0700

    complete client login and register

--- client/src/containers/Public/Login.js
+++ client/src/containers/Public/Login.js
@@ -1,16 +1,29 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Button, InputForm } from '../../components'
+import { Link } from 'react-router-dom'
+import { useLocation } from 'react-router-dom'
+
+
 
 const Login = () => {
+
+  const location = useLocation()
+  const [IsRegister, setIsRegister] = useState(location.state?.flag)
+  useEffect(() => {
+    setIsRegister(location.state?.flag)
+  }, [location.state?.flag])
+  console.log(location)
+
   return (
     <div className='bg-white w-[600px] p-[30px] pb-[100px] rounded-md shadow-sm'>
-      <h3 className='font-semibold text-2xl mb-3'> Đăng nhập</h3>
+      <h3 className='font-semibold text-2xl mb-3'> {IsRegister ? 'Đăng kí tài khoản' : 'Đăng nhập'}</h3>
       <div className='w-full flex flex-col gap-3'>
-        <InputForm label={'Số điện thoại'} />
-        <InputForm label={'Mật khẩu'} />
+        {IsRegister && <InputForm label={'HỌ VÀ TÊN'} />}
+        <InputForm label={'SỐ ĐIỆN THOẠI'} />
+        <InputForm label={'MẬT KHẨU'} />
 
         <Button
-          text='Đăng nhập'
+          text={IsRegister ? 'Tạo tài khoản' : 'Đăng nhập'}
           bgColor='bg-secondary1'
           textColor='text-white'
           fullWidth
@@ -18,8 +31,14 @@ const Login = () => {
 
       </div>
       <div className='mt-7 flex items-center justify-between' >
-        <small className='text-[blue] hover:text-[red] cursor-pointer'>Bạn quên mật khẩu?</small>
-        <small className='text-[blue] hover:text-[red] cursor-pointer'>Tạo tài khoản mới </small>
+        {IsRegister
+          ? <small>Bạn đã có tài khoản ? <span onClick={() => { setIsRegister(false) }} className='text-blue-500 hover:underline cursor-pointer'>Đăng nhập ngay</span></small>
+          : <>
+            <small className='text-[blue] hover:text-[red] cursor-pointer'>Bạn quên mật khẩu?</small>
+            <small onClick={() => { setIsRegister(true) }} className='text-[blue] hover:text-[red] cursor-pointer'>Tạo tài khoản mới </small>
+          </>
+        }
+
       </div>
     </div>
 
