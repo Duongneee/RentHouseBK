44937c8d9b87abec0ebf582f0f6178f203282bf8
Duongneee <duongquach2k3@gmail.com>
2024-10-30 16:05:25 +0700

    fix connect data basse of branch

--- client/src/containers/Public/Login.js
+++ client/src/containers/Public/Login.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { Button, InputForm } from '../../components'
 import { Link } from 'react-router-dom'
+import { apiRegister } from '../../services/auth'
 import { useLocation } from 'react-router-dom'
 
 
@@ -9,24 +10,38 @@ const Login = () => {
 
   const location = useLocation()
   const [IsRegister, setIsRegister] = useState(location.state?.flag)
+
+  const [payLoad, setpayLoad] = useState({
+    phone: '',
+    password: '',
+    name: ''
+  })
+
   useEffect(() => {
     setIsRegister(location.state?.flag)
   }, [location.state?.flag])
   console.log(location)
 
+  const handleSubmit = async () => {
+    console.log(payLoad)
+    const response = await apiRegister(payLoad)
+    console.log(response)
+  }
+
   return (
     <div className='bg-white w-[600px] p-[30px] pb-[100px] rounded-md shadow-sm'>
       <h3 className='font-semibold text-2xl mb-3'> {IsRegister ? 'Đăng kí tài khoản' : 'Đăng nhập'}</h3>
       <div className='w-full flex flex-col gap-3'>
-        {IsRegister && <InputForm label={'HỌ VÀ TÊN'} />}
-        <InputForm label={'SỐ ĐIỆN THOẠI'} />
-        <InputForm label={'MẬT KHẨU'} />
+        {IsRegister && <InputForm label={'HỌ VÀ TÊN'} value={payLoad.name} setValue={setpayLoad} type='name' />}
+        <InputForm label={'SỐ ĐIỆN THOẠI'} value={payLoad.phone} setValue={setpayLoad} type='phone' />
+        <InputForm label={'MẬT KHẨU'} value={payLoad.password} setValue={setpayLoad} type='password' />
 
         <Button
           text={IsRegister ? 'Tạo tài khoản' : 'Đăng nhập'}
           bgColor='bg-secondary1'
           textColor='text-white'
           fullWidth
+          onClick={handleSubmit}
         />
 
       </div>
