5b9d698120c7978d684c36ef23eef82c622c1765
duong204736 <152875418+duong204736@users.noreply.github.com>
2024-11-01 17:42:28 +0700

    fix typos

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,13 +1,13 @@
 import authRouter from "./auth"
 import categoryRouter from './category'
 
-import insertRoute from './data-insert';
+import insertRouter from './data-insert';
 
 const initRoutes = (app) => {
 
-    app.use('/api/v1/auth', authRouter) 
-    app.use('/api/v1/data-insert', insertRoute) 
-    app.use('/api/v1/category', categoryRouter) 
+    app.use('/api/v1/auth', authRouter)
+    app.use('/api/v1/data-insert', insertRouter)
+    app.use('/api/v1/category', categoryRouter)
 
     return app.use('/', (req, res) => {
         res.send('Server is runnning')
