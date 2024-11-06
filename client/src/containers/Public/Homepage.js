a5e61f8523c98b1afa0a8319eeb1b379241b0742
Duong1209 <130141042+Duong1209@users.noreply.github.com>
2024-11-06 13:06:07 +0700

    6-11

--- client/src/containers/Public/Homepage.js
+++ client/src/containers/Public/Homepage.js
@@ -5,8 +5,7 @@ import {List, Pagination} from './index'
 import {  useSearchParams } from 'react-router-dom'
 
 const Homepage = () => {
-  const [params] = useSearchParams()
-  console.log(params.get('page'));
+  const [params] = useSearchParams()  
   return (
     <div className='w-full flex flex-col gap-3'>
       <div>
@@ -16,8 +15,8 @@ const Homepage = () => {
       <Province />
       <div className='w-full flex gap-4'>
       <div className='w-[70%]'>
-        <List />
-        <Pagination number={params.get('page')} />
+        <List page={params.get('page')} />
+        <Pagination page={params.get('page')} />
       </div>
       <div className='w-[30%] flex flex-col gap-4 justify-start items-center'>
         <ItemSidebar />
