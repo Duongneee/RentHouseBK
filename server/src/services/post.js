143adcfd28d8dbe186f60fef10fdb1d0c0ea11d3
duong204736 <152875418+duong204736@users.noreply.github.com>
2024-11-07 18:16:45 +0700

    Fix to new database scheme

--- server/src/services/post.js
+++ server/src/services/post.js
@@ -1,21 +1,21 @@
 import { raw } from 'express'
 import db from '../models'
 
-export const getPostsService = () => new Promise(async(resolve, reject) => {
-    try{
+export const getPostsService = () => new Promise(async (resolve, reject) => {
+    try {
         const response = await db.Post.findAll({
-            raw : true,
+            raw: true,
             include: [
-                { model: db.user, as: 'user', attributes: ['name', 'phone'] },
+                { model: db.User, as: 'owner', attributes: ['name', 'phone'] },
             ],
-            attributes : ['id', 'title', 'star','priceRange', 'sizeRange', 'cityId', 'districtId', 'description' ]
+            attributes: ['id', 'title', 'star', 'priceRange', 'sizeRange', 'city', 'district', 'description']
         })
         resolve({
-            err: response ? 0:1,
-            msg: response ? 'OK':'Failed to get posts.',
+            err: response ? 0 : 1,
+            msg: response ? 'OK' : 'Failed to get posts.',
             response
         })
-    } catch(error) {
+    } catch (error) {
         reject(error)
     }
 })
