54f954468b6d1f7480e2202aa01e1d739dfc2426
duong204736 <duong.tv204736@gmail.com>
2024-11-21 21:33:49 +0700

    support unlimit price and size range

--- server/src/controllers/post.js
+++ server/src/controllers/post.js
@@ -24,9 +24,15 @@ export const postFilter = async (req, res) => {
             filters.price = {
                 [Op.between]: [parseInt(req.query.priceFrom, 10), parseInt(req.query.priceTo, 10)]
             }
+        } else if (req.query.priceFrom) {
+            console.log('Controller.PostFilter.PriceFrom: ', req.query.priceFrom)
+            filters.price = { [Op.gte]: parseInt(req.query.priceFrom, 10) }
         }
-        if (req.query.sizeFrom && req.query.sizeTo)
-            filters.size = { [Op.between]: [req.query.sizeFrom, req.query.sizeTo] }
+        if (req.query.sizeFrom && req.query.sizeTo) {
+            filters.size = { [Op.between]: [parseInt(req.query.sizeFrom), parseInt(req.query.sizeTo)] }
+        } else if (req.query.sizeFrom) {
+            filters.size = { [Op.gte]: parseInt(req.query.sizeFrom, 10) }
+        }
         if (req.query.categoryCode) filters.categoryCode = req.query.categoryCode
         console.log('Controller.PostFilter.Filters: ', filters)
         const response = await postService.postFilterService(filters, req.query.page)
@@ -55,14 +61,14 @@ export const getPostsLimit = async (req, res) => {
 }
 
 export const getPostById = async (req, res) => {
-    const { id } = req.params; 
-    try {      
+    const { id } = req.params;
+    try {
         const response = await postService.getPostByIdService(id)
         return res.status(200).json(response)
     } catch (error) {
         return res.status(500).json({
             err: 1,
-            msg: 'Failed to get post controller: '+ error
+            msg: 'Failed to get post controller: ' + error
         });
     }
 };
@@ -74,21 +80,21 @@ export const getNewPosts = async (req, res) => {
     } catch (error) {
         return res.status(500).json({
             err: -1,
-            msg: 'Failed to get post controller: '+ error
+            msg: 'Failed to get post controller: ' + error
         })
     }
 }
 
 export const createNewPost = async (req, res) => {
     try {
-        const {  title, price, size} = req.body
+        const { title, price, size } = req.body
         const { id } = req.user
-        if ( !title || !id || !price || !size )
+        if (!title || !id || !price || !size)
             return res.status(400).json({
                 err: -1,
                 msg: 'Missing input'
             })
-        
+
         const response = await postServive.createNewPostService(req.body, id)
         return res.status(200).json(response)
     } catch (error) {
