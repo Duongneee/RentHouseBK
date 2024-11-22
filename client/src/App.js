0f7bc22a3c558d708c54b3bdac9c19dc5cd021fb
duong204736 <152875418+duong204736@users.noreply.github.com>
2024-11-23 01:16:09 +0700

    fix category on header

--- client/src/App.js
+++ client/src/App.js
@@ -23,10 +23,10 @@ function App() {
           <Route path='*' element={<Homepage />} />
           <Route path={path.HOME__PAGE} element={<Homepage />} />
           <Route path={path.LOGIN} element={<Login />} />
-          <Route path={path.CHO_THUE_CAN_HO} element={<Categories category={"CTCH"} />} />
-          <Route path={path.NHA_CHO_THUE} element={<Categories category={"NCT"} />} />
-          <Route path={path.CHO_THUE_PHONG_TRO} element={<Categories category={"CTPT"} />} />
-          <Route path={path.CHO_THUE_MAT_BANG} element={<Categories category={"CTMB"} />} />
+          <Route path={path.CHO_THUE_CAN_HO} element={<Categories categoryCode={"CTCH"} />} />
+          <Route path={path.NHA_CHO_THUE} element={<Categories categoryCode={"NCT"} />} />
+          <Route path={path.CHO_THUE_PHONG_TRO} element={<Categories categoryCode={"CTPT"} />} />
+          <Route path={path.CHO_THUE_MAT_BANG} element={<Categories categoryCode={"CTMB"} />} />
           <Route path={path.DETAIL_POST__TITLE__POSTID} element={<DetailPost />} />
           <Route path={'chi-tiet/'} element={<DetailPost />} />
           <Route path={path.FILTER} element={<FilterResult />} />
