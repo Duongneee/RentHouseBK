1b4acaa38773fcff6d29ffed65fa6a92714b8b2d
duong204736 <152875418+duong204736@users.noreply.github.com>
2024-11-29 18:39:16 +0700

    backend for bookmark

--- server/src/routes/user.js
+++ server/src/routes/user.js
@@ -6,6 +6,9 @@ const router = express.Router()
 
 router.use(verifyToken)
 router.get('/get-current', userController.getCurrent)
+router.get('/get-bookmarked-posts', userController.getBookmarkedPosts)
+router.post('/create-bookmark', userController.createBookmark)
+router.delete('/delete-bookmark', userController.deleteBookmark)
 
 
 export default router
