3b28bbd3a59f377a82fc898e35ad1499f16e8fcf
duong204736 <152875418+duong204736@users.noreply.github.com>
2024-12-06 21:52:53 +0700

    Fix price display

--- client/src/components/Sitem.js
+++ client/src/components/Sitem.js
@@ -3,27 +3,28 @@ import moment from 'moment'
 import 'moment/locale/vi'
 import { Link } from 'react-router-dom'
 import { path } from '../untils/constant'
+import { shortenMoneyAmount } from '../untils/moneyShorten'
 
-const Sitem = ({title, price, image, createdAt, id}) => {
+const Sitem = ({ title, price, image, createdAt, id }) => {
 
   const formatTime = (createdAt) => {
     return moment(createdAt).fromNow()
-}
+  }
 
   return (
     <div className='w-full flex items-center gap-2 py-2 border-b border-gray-300'>
-      <img  src={image[0]}
-      //src="https://pt123.cdn.static123.com/images/thumbs/450x300/fit/2024/11/01/z5988970599282-eba7976461c5968368d6e7d6df677f9a_1730458316.jpg" 
-      alt="anh" 
-      className='w-[65px] h-[65px] object-cover flex-none rounded-md'
+      <img src={image[0]}
+        //src="https://pt123.cdn.static123.com/images/thumbs/450x300/fit/2024/11/01/z5988970599282-eba7976461c5968368d6e7d6df677f9a_1730458316.jpg" 
+        alt="anh"
+        className='w-[65px] h-[65px] object-cover flex-none rounded-md'
       />
       <div className='w-full flex-auto flex flex-col justify-between gap-1'>
-        <Link to={`${path.DETAIL}${id}`} className='text-blue-600 text-[14px]'>{`${title?.slice(0,45)}...`}</Link>
+        <Link to={`${path.DETAIL}${id}`} className='text-blue-600 text-[14px]'>{`${title?.slice(0, 45)}...`}</Link>
         <div className='flex items-center justify-between w-full'>
-            <span className='text-sm font-medium text-green-500'>{price} triệu/tháng</span>
-            <span className='text-sm text-gray-500'>{formatTime(createdAt)}</span>
+          <span className='text-sm font-medium text-green-500'>{shortenMoneyAmount(price)} /tháng</span>
+          <span className='text-sm text-gray-500'>{formatTime(createdAt)}</span>
         </div>
-     </div>
+      </div>
     </div>
   )
 }
