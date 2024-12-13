8f50012a1fb3ebd23c7f70ca16467c83c8b0ae67
Duongneee <duongquach2k3@gmail.com>
2024-12-14 02:45:45 +0700

    test1

--- server/server.js
+++ server/server.js
@@ -11,19 +11,6 @@ app.use(cors({
     methods: ["POST", 'GET', 'PUT', "DELETE"]
 }))
 
-// // Cấu hình để phục vụ các file tĩnh từ thư mục build của React
-// app.use(express.static(path.join(__dirname, '../client/build')))
-
-// // Đảm bảo tất cả các route không tìm thấy đều trả về file index.html của React
-// app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname, '../client/build', 'index.html'))
-// })
-
-app.get('*', (req, res) => {
-    // Chuyển hướng tất cả yêu cầu tới ứng dụng React trên Render
-    res.redirect('https://duongquach.id.vn');
-});
-
 // server có thể đọc được dữ liệu API từ client gửi lên
 app.use(express.json())
 app.use(express.urlencoded({ extends: true }))
